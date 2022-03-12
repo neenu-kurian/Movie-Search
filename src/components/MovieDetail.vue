7d3e3abc952061dd6dbc05a710d577b9a4bf5637
Neenu <neenu.kurian@ev-box.com>
2022-03-12 14:28:26 +0100

    added movie api

--- src/components/MovieDetail.vue
+++ src/components/MovieDetail.vue
@@ -1,17 +1,30 @@
 <template>
-    <div>Movie Detail</div>
+  <div>
+    <img :src="movie.Poster" alt="movie" />
+    <div>{{movie.Title}}</div>
+    <div>{{movie.Type}}. {{movie.Year}}</div>
+    <div>Rating: {{movie.Ratings[0].Value}}</div>
+    <div>{{movie.Genre}}</div>
+    <div>Director {{movie.Director}}</div>
+    <div>Actors {{movie.Actors}}</div>
+  </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
 export default {
-    name: "MovieDetail",
-    mounted() {
-        this.fetchMovieDetails();
+  name: "MovieDetail",
+  computed: {
+    ...mapGetters(["movie"]),
+  },
+  mounted() {
+    this.fetchMovieDetails();
+  },
+  methods: {
+    fetchMovieDetails() {
+      this.$store.dispatch("fetchMovieDetails", this.$route.params.id);
     },
-    methods: {
-        fetchMovieDetails() {
-            this.$store.dispatch("fetchMovieDetails",this.$route.params.id);
-        }
-    }
-}
+  },
+};
 </script>
