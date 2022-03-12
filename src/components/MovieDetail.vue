a3f180eaa50d6f28f2fe376fa019f2bc4f244565
Neenu <neenu.kurian@ev-box.com>
2022-03-12 16:03:03 +0100

    added styles

--- src/components/MovieDetail.vue
+++ src/components/MovieDetail.vue
@@ -7,11 +7,23 @@
     <div class="right">
       <div class="detail__wrapper">
         <h1 class="detail__title">{{ movie.Title }}</h1>
-        <div class="detail__year">{{ movie.Type }}. {{ movie.Year }}</div>
-        <div>Rating: {{ movie.Ratings[0].Value }}</div>
-        <div>{{ movie.Genre }}</div>
-        <div>Director {{ movie.Director }}</div>
-        <div>Actors {{ movie.Actors }}</div>
+        <span>{{ movie.Ratings[0].Value }}</span>
+        <div class="detail__year">{{ movie.Type }} | {{ movie.Year }}</div>
+        <div class="detail__overview">
+          <h2 class="detail__text">OVERVIEW</h2>
+          <div class="detail__cast">
+            <span class="detail__movie--text">Genre:</span>
+            {{ movie.Genre }}
+          </div>
+          <div class="detail__cast">
+            <span class="detail__movie--text">Created by:</span>
+            {{ movie.Director }}
+          </div>
+          <div class="detail__cast">
+            <span class="detail__movie--text">Starring:</span>
+            {{ movie.Actors }}
+          </div>
+        </div>
       </div>
     </div>
   </div>
